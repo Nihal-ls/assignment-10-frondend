728eccdaaad85b5f60ffdfa74c9a25fada6693d6
Nihal <nihallaskar888@gmail.com>
2025-11-10 02:02:26 +0600

    added a footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,20 @@ import logo from '../assets/logo.jpg'
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="footer footer-horizontal bg-gradient-to-r  from-sky-300 to-blue-400 footer-center  text-primary-content p-10">
+                <aside>
+                    <img
+                        className='w-20 h-20 rounded-full'
+                        src={logo} alt="" />
+                    <p className="font-bold">
+                        Habit Tracker
+                        <br />
+                        A web app for users to create, track, and manage daily habits to build streaks and boost
+                        productivity.
+                    </p>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                </aside>
+            </footer>
         </div>
     );
 };
