8072b0f8def591a643453db00f280424c11d59f0
Nihal <nihallaskar888@gmail.com>
2025-11-09 17:17:29 +0600

    added a banner and why build habit section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router';
+import logoImg from '../assets/logo.jpg'
 const Navbar = () => {
 
- 
+    const Navlinks = <>
+        <li className='text-lg'><NavLink to='/'>Home</NavLink></li>
+        <li className='text-lg '><NavLink to='/all-habits'>Browse Public Habits</NavLink></li>
+    </>
 
     return (
-       <div className="">
+        <div>
+            <div className="navbar bg-base-100 container mx-auto rounded-full py-3 px-6 mt-5 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {Navlinks}
+                        </ul>
+                    </div>
+                    <div className="items-center flex">
+                        <img className='w-[70px]' src={logoImg} alt="" />
+                        <a className="btn btn-ghost text-xl font-bold">
+                            Habit Tracker
+                        </a>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {Navlinks}
+                    </ul>
+                </div>
+                <div className="navbar-end gap-3">
+                    <a className="btn  px-7 hover:text-white hover:font-bold hover:bg-gradient-to-r  from-sky-300 to-blue-400   ">Login</a>
+                    <a className="btn 
+                     text-white font-bold bg-gradient-to-r  from-sky-300 to-blue-400  px-7 ">Signup</a>
+                </div>
+            </div>
+        </div>
 
-       </div>
     );
 };
 
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import bannerbg from '../assets/bannerBg.jpg'
+import { motion } from "framer-motion";
+import { FaClock, FaHeart, FaRegCheckCircle, FaRocket } from 'react-icons/fa';
 
 const Home = () => {
     return (
         <div>
-            
+            <div
+                className=" mt-20"
+               
+            >
+                <div className="w-full relative h-100 bg-cover bg-no-repeat  flex justify-center flex-col text-center"
+                 style={{ backgroundImage: `url(${bannerbg})` }}
+                >
+                    <motion.h1
+                        className="text-4xl font-bold text-blue-400"
+                        initial={{ opacity: 0, y: -55 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, ease: "easeOut" }}
+                    >
+                        Build Better Habits, With Us!
+                    </motion.h1>
+                    <motion.p
+                        className="mt-3 text-gray-500 text-lg "
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.5, ease: "easeOut" }}
+                    >
+                        Stay consistent, track your progress, and achieve your <br /> goals effortlessly.
+                        Our habit tracker helps <br /> you turn small actions into lasting routines,
+                        keeping you motivated every step of the way.
+                    </motion.p>
+                </div>
+            </div>
+            {/* Why Build Habits? */}
+            <div className="container mx-auto ">
+                <h1 className='text-3xl text-center font-bold mt-10'>Why Build Habits?</h1>
+                <p className='text-center mt-2 text-gray-900'>Small daily habits lead to big results. Here's why building habits matters:</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center mt-2 gap-2">
+                    <div className="card w-80 mx-auto bg-base-100 my-auto  shadow-sm hover:shadow-lg transition-shadow duration-300">
+                        <div className="card-body ">
+                            <h2 className="card-title"><FaRegCheckCircle size={25} color='green' /> Consistency</h2>
+                            <p>Stay on track and make progress every day by tracking small actions.</p>
+                        </div>
+                    </div>
+                    <div className="card w-80 mx-auto bg-base-100 my-auto  shadow-sm hover:shadow-lg transition-shadow duration-300">
+                        <div className="card-body">
+                            <h2 className="card-title"><FaRocket color='blue' size={25}/> Motivation</h2>
+                            <p>Visualize your progress to stay motivated and never lose focus.</p>
+                        </div>
+                    </div>
+                    <div className="card w-80 mx-auto bg-base-100 my-auto  shadow-sm hover:shadow-lg transition-shadow duration-300">
+                        <div className="card-body">
+                            <h2 className="card-title"><FaHeart  color='red' size={25}/> Wellbeing</h2>
+                            <p>Healthy habits improve your mind and body over time.</p>
+                        </div>
+                    </div>
+                    <div className="card w-80 mx-auto bg-base-100 my-auto  shadow-sm hover:shadow-lg transition-shadow duration-300">
+                        <div className="card-body">
+                            <h2 className="card-title"><FaClock color='blue' size={25}/> Productivity</h2>
+                            <p>Turn small daily actions into big achievements efficiently.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
